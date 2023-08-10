82a96773f884dfbfc64136cf2c6755f3fab86a94
Agnieszka Staniszewska <agnieszka.czupryna@wp.pl>
2023-08-10 18:02:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/checklist/fragments/SignUpFragment.kt
@@ -0,0 +1,71 @@
+package com.example.checklist.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.checklist.R
+import com.example.checklist.databinding.FragmentSignUpBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUpFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navControl: NavController
+    private lateinit var binding: FragmentSignUpBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        registerEvents()
+    }
+
+    private fun init(view:View){
+        navControl = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun registerEvents(){
+
+        binding.authTV.setOnClickListener{
+            navControl.navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+
+        binding.nextBtn.setOnClickListener {
+            val email = binding.emailET.text.toString().trim()
+            val pass = binding.passET.text.toString().trim()
+            val verifyPass = binding.repassET.text.toString().trim()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && verifyPass.isNotEmpty())
+                if (pass == verifyPass){
+                    auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(
+                        OnCompleteListener {
+                            if (it.isSuccessful){
+                                Toast.makeText(context, "Pomyślnie zarejestrowano.", Toast.LENGTH_SHORT).show()
+                                navControl.navigate(R.id.action_signUpFragment_to_homeFragment)
+                            }else{
+                                Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+                            }
+                        })
+                }
+        }
+    }
+
+}
